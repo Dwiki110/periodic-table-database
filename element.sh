19ecfdb897ac77a58e60bb3da7643361bac6d2ca
Dwiki110 <152857022+Dwiki110@users.noreply.github.com>
2026-01-21 17:46:38 +0700

    Add script to query periodic table elements

---
+++ element.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  # Cek apakah input adalah angka (atomic number)
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1")
+  else
+    # Jika bukan angka, cek apakah input adalah String (Symbol atau Name)
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
+  fi
+
+  # Cek apakah elemen ditemukan
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    # Parse hasil query (dipisahkan oleh tanda | secara default di psql -t --no-align)
+    echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    done
+  fi
+fi
+# fix comment
+# refactor comment
+# chore comment
+# test comment
